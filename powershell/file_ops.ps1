8da961e2125a347b352249ac9f918afe36c5368e
0xdomyz <yzdominic@outlook.com>
2024-02-17 21:18:55 +1100

    udpate file ops

--- powershell/file_ops.ps1
+++ powershell/file_ops.ps1
@@ -2,3 +2,18 @@
 Set-Content -Path .\test.txt -Value "Hello, World!"
 $content = Get-Content -Path .\test.txt
 Write-Output $content
+
+# Rename the file
+Rename-Item -Path .\test.txt -NewName test2.txt
+
+# Copy the file
+Copy-Item -Path .\test2.txt -Destination .\test.txt
+
+# Delete the file
+Remove-Item -Path .\test.txt
+
+# Move the file
+Move-Item -Path .\test2.txt -Destination .\test.txt
+
+# start windows explorer
+Start-Process .
