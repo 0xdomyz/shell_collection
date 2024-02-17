06a751dae0f1b82ce20e9174b14352ec25f4288b
0xdomyz <yzdominic@outlook.com>
2024-02-17 21:20:17 +1100

    update dir ops

--- powershell/dir_ops.ps1
+++ powershell/dir_ops.ps1
@@ -1,3 +1,18 @@
 # Get the files in the current directory
 $files = Get-ChildItem -Path .\
 Write-Output $files.Name
+
+# pwd
+Write-Output (Get-Location).Path
+
+# pushd
+Push-Location
+
+# cd
+Set-Location -Path 'E:\music\'
+
+# popd
+pop-Location
+
+# ls
+Get-ChildItem -Path .\
