9aa6e714820345fe83f1fc8792dcaec8e659efa5
Dom Z <91247640+0xdomyz@users.noreply.github.com>
2025-11-16 17:01:27 +0000

    Refactor SFTP script for key-based authentication
    Updated SFTP script to use key-based authentication and simplified commands.

--- bash/sftp_example.sh
+++ bash/sftp_example.sh
@@ -1,27 +1,6 @@
-# Variables
-USER='user'
-HOST='192.168.0.1'
-PORT='22'
-
-# Upload a file to the remote server
-###########################################
-echo "test" > test_file.csv
-
-sftp -oPort=$PORT $USER@$HOST <<EOF
-put test_file.csv test_file.csv
+sftp -i "a_pem.pem" ubuntu@ec2-{ip address of amz ec2}.compute-1.amazonaws.com
+ls # look at remote
+lls # look at local
+put local_file_path remote_file_path
+get remote_file_path local_file_path
 exit
-EOF
-
-cat test_file.csv # List the files on the remote server
-
-
-# Download a file from the remote server
-###########################################
-echo test2 > test_file2.csv
-
-sftp -oPort=$PORT $USER@$HOST <<EOF
-get test_file2.csv
-exit
-EOF
-
-cat test_file2.csv
